5c533f2be8bb20a2faa37937a8d39770c7d07283
Dermot Boyle <dermotb@hotmail.com>
2021-02-25 21:30:09 +0000

    First client methods added

---
+++ ForumClient/Models/userPost.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Models
+{
+    public class userPost
+    {
+        public int ID { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        [MinLength(1)]
+        [MaxLength(25)]
+        public string Subject { get; set; }
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string Message { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Time: {0} | ID: {1} | Subject: {2} | Message: {3}", TimeStamp.TimeOfDay, ID, Subject, Message);
+        }
+
+    }
+}
---
+++ ForumClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using Forum.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ForumClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAllPosts().Wait();
+        }
+
+        private static async Task GetAllPosts()
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://localhost:53368/");
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("api/Forum");
+
+            if(response.IsSuccessStatusCode)
+            {
+                IEnumerable<userPost> posts = await response.Content.ReadAsAsync<IEnumerable<userPost>>();
+
+                foreach (userPost usp in posts)
+                {
+                    Console.WriteLine(usp.ToString());
+                }
+            }
+
+
+
+
+        }
+    }
+}
